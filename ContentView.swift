69d819c944a0c7cb99cc32b67114b8d0d19738ec
Ipsky <yap@usc.edu>
2021-04-01 15:57:22 -0700

    break out

--- ContentView.swift
+++ ContentView.swift
@@ -37,36 +37,3 @@ struct ContentView: View {
     }
 }
 
-struct MarkdownView: UIViewRepresentable {
-    
-    typealias UIViewType = PMTextView
-    
-    private let textView = UIViewType()
-    
-    public var editMode: EditMode
-    
-    public let textPub: AnyPublisher<String, Never>
-    
-    @Binding var text: String
-    
-    func makeUIView(context: Context) -> UIViewType {
-        /// load provided document markdown
-        textView.text = text
-        
-        /// respond to user changes in Font Size
-        /// source: https://stackoverflow.com/a/57449881/12395667
-        textView.adjustsFontForContentSizeCategory = true
-        
-        textView.setup(
-            text: textPub
-        )
-        
-        return textView
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        uiView.text = text
-        
-    }
-}
-
---
+++ MarkdownView.swift
@@ -0,0 +1,44 @@
+//
+//  MarkdownView.swift
+//  CombineTest
+//
+//  Created by Secret Asian Man Dev on 1/4/21.
+//
+
+import SwiftUI
+import Combine
+
+struct MarkdownView: UIViewRepresentable {
+    
+    typealias UIViewType = PMTextView
+    
+    private let textView = UIViewType()
+    
+    public var editMode: EditMode
+    
+    public let textPub: AnyPublisher<String, Never>
+    
+    @Binding var text: String
+    
+    func makeUIView(context: Context) -> UIViewType {
+        /// load provided document markdown
+        textView.text = text
+        
+        /// respond to user changes in Font Size
+        /// source: https://stackoverflow.com/a/57449881/12395667
+        textView.adjustsFontForContentSizeCategory = true
+        
+        textView.setup(
+            text: textPub
+        )
+        
+        return textView
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        uiView.text = text
+        
+    }
+}
+
+
